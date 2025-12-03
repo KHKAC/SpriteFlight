a344ed1ccfc89b09eb42100c75767a977b9f1da6
KHKAmuta <rudgnsrb888@naver.com>
2025-12-03 21:56:18 +0900

    change

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,18 +6,21 @@ public class Obstacle : MonoBehaviour
     public float maxSpeed = 150f;
     public float minSize = 0.5f;
     public float maxSize = 2.0f;
-    public float maxSpinSpeed = 100f;
+    public float maxSpinSpeed = 10f;
     Rigidbody2D rb;
+
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize);
-        transform.localScale = new Vector3(randomSize, randomSize, 1);
+        transform.localScale =
+            new Vector3(randomSize, randomSize, 1);
         
         Vector2 randomDirection = Random.insideUnitCircle;
         float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
-        
+
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(randomDirection * randomSpeed);
+        
         float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
         rb.AddTorque(randomTorque);
     }
